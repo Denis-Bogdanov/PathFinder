f05f68e0ddcb438c6d85dedca494f6acb9aa0201
milten15 <stefan_skellen@mail.ru>
2019-10-02 20:55:45 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private NavMeshAgent _nav;
+
+
+    void Start()
+    {
+        _nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Camera.main != null)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    _nav.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
